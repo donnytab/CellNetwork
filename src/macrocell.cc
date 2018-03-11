d486e1ad2c5236636e116b26f9ddba8d9edb30d3
downson <downson.yuen@gmail.com>
2018-03-11 00:08:12 -0500

    Add signals to process message time

--- src/macrocell.cc
+++ src/macrocell.cc
@@ -11,10 +11,21 @@ class MacroCell : public cSimpleModule
 {
 private:
     cArray storeArray;
+    int timeSensitiveCount;
+    int timeMediumCount;
+    int timeTolerantCount;
+    simtime_t timeSensitiveTimeAverage;
+    simtime_t timeMediumTimeAverage;
+    simtime_t timeTolerantTimeAverage;
     simsignal_t msgTimeSignal;
+    simsignal_t timeSensitiveQueueMsgTimeSignal;    // high priority, priority equals 0
+    simsignal_t timeMediumQueueMsgTimeSignal;   // medium priority, priority equals 1
+    simsignal_t timeTolerantQueueMsgTimeSignal; // low priority, priority equals 2
 protected:
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
+    virtual void finish() override;
+    void processMessageTime(int priority, simtime_t time);
 };
 
 Define_Module(MacroCell);
@@ -23,6 +34,12 @@ void MacroCell::initialize()
 {
     storeArray = cArray("engergyDataStore");
     msgTimeSignal = registerSignal("messageTime");
+    timeSensitiveQueueMsgTimeSignal = registerSignal("timeSensitiveQueueMsgTime");
+    timeMediumQueueMsgTimeSignal = registerSignal("timeMediumQueueMsgTime");
+    timeTolerantQueueMsgTimeSignal = registerSignal("timeTolerantQueueMsgTime");
+    timeSensitiveCount = 0;
+    timeMediumCount = 0;
+    timeTolerantCount = 0;
 }
 
 void MacroCell::handleMessage(cMessage *msg)
@@ -30,7 +47,31 @@ void MacroCell::handleMessage(cMessage *msg)
     if(!strcmp(msg->getName(), ENERGY_MESSAGE_TYPE)) {
         EnergyMsg *eMsg = check_and_cast<EnergyMsg*>(msg);
         eMsg->setEndTimestamp(simTime());
-        emit(msgTimeSignal, simTime() - eMsg->getStartTimestamp());
+        processMessageTime(eMsg->getPriority(), simTime() - eMsg->getStartTimestamp());
+//        emit(msgTimeSignal, simTime() - eMsg->getStartTimestamp());
         storeArray.add(check_and_cast<cObject*>(msg));
+    }
+}
+
+void MacroCell::finish() {
+    emit(timeSensitiveQueueMsgTimeSignal, (double)timeSensitiveTimeAverage.dbl()/timeSensitiveCount);
+    emit(timeMediumQueueMsgTimeSignal, (double)timeMediumTimeAverage.dbl()/timeMediumCount);
+    emit(timeTolerantQueueMsgTimeSignal, (double)timeTolerantTimeAverage.dbl()/timeTolerantCount);
+}
+
+void MacroCell::processMessageTime(int priority, simtime_t time) {
+    switch(priority) {
+        case 0:
+            timeSensitiveTimeAverage += time;
+            timeSensitiveCount++;
+            break;
+        case 1:
+            timeMediumTimeAverage += time;
+            timeMediumCount++;
+            break;
+        case 2:
+            timeTolerantTimeAverage += time;
+            timeTolerantCount++;
+            break;
     }
 }
